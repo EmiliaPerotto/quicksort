e26c68ffa5f1b6cc57b5085171f9f272ae1825de
Emilia Perotto <100068661@csisd.org>
2025-01-29 14:59:36 -0600

    dfg

---
+++ QuickSort.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+public class QuickSort{
+  public static void partition(int[] arr1){
+    int length = arr1.length;
+    int pivot = quick(arr1)
+    int pivot1 = arr[quick(arr1)];
+    if(length <= 1){
+      return;
+    } //base case
+    int o = 0
+    int b = 0;
+    int temp1 = 0;
+    int temp2 = 0;
+    for(int i = 0; i < arr1.legnth; i++){
+        if(arr1[o] < pivot){
+          b++
+        }
+      }
+    }
+  public static int quick(int[] arr){
+   int pivot = arr.length - 1;
+   return pivot;
+  }
+  public static void main (String args[]) throws IOException{
+    Scanner in = new Scanner(System.in);
+    System.out.println("Input filename: ");
+    String filename = in.nextLine();
+    Scanner fin = new Scanner(new File(filename));
+    int lines = fin.nextInt();
+    int[] arr = new int[lines];
+    int j = 0;
+    while(fin.hasNextLine()){
+      arr[j] = fin.nextInt();
+      j++;
+    }
+    for (int i = 0; i < arr.length; i++){
+      if(i != arr.length -1){
+        System.out.print(arr[i] + " , ");
+      }
+      else{
+        System.out.println(arr[i]);
+    }
+  }
+    System.out.println(arr[quick(arr)]);//pivot
+  }
+  }
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# quicksort
+# Quick Sort
+
+## Due: Mon 2/3 at 11:59 PM
+
+### No Googling the quick sort algorithm
+
+- Create a program called `QuickSort.java`
+- Create a void static method that implements the quick sort algorithm
+  - For this, you will also need to create a static method that partitions the array and returns the index of the pivot value after it has been partitioned
+- In the main method:
+  - Prompt the user to enter a filename containing the data to read in
+  - Read from that file
+  - The first line is the number of elements the file contains
+  - Read the remaining lines into an array
+  - Use the quick sort method you wrote to sort the array
+  - Print the sorted array
+
+***Example Input:***\
+input1.txt\
+***Example Output:***\
+1, 2, 3, 7, 17, 19, 24, 30, 44, 85
